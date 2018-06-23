1749c2d258d98b69935eda63c8dd2e85795b6829
Porawit Poboonma <ball6847@gmail.com>
2018-06-23 11:40:53 +0700

    add mgo

--- main.go
+++ main.go
@@ -3,13 +3,46 @@ package main
 import (
 	"net/http"
 
+	"github.com/globalsign/mgo"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+type accesToken struct {
+	Token     string `json:"accessToken"`
+	ExpiresIn int    `json:"expiresIn"`
+}
+
+type SignupPayload struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func signup(c echo.Context) (err error) {
+	payload := new(SignupPayload)
+
+	if err = c.Bind(payload); err != nil {
+		c.Error(err)
+	}
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		c.Error(err)
+	}
+
+	collection := session.DB("odds").C("credentials")
+
+	err = collection.Insert(payload)
+	if err != nil {
+		c.Error(err)
+	}
+
+	return c.JSON(http.StatusOK, payload)
 }
 
 func main() {
@@ -22,6 +55,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.POST("/signup", signup)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
